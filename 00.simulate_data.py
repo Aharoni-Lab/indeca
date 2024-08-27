00be469d8e74127c255b51148c33567f711f3173
phildong <35715936+phildong@users.noreply.github.com>
2024-08-27 16:22:33 -0400

    add simulation of both original and upsampled data

--- 00.simulate_data.py
+++ 00.simulate_data.py
@@ -5,7 +5,7 @@ import numpy as np
 
 from routine.simulation import generate_data
 
-OUT_PATH = "./intermediate/simulated/simulated.nc"
+OUT_PATH = "./intermediate/simulated/"
 PARAM_TAU_D = 6
 PARAM_TAU_R = 1
 PARAM_UPSAMP = 10
@@ -13,26 +13,27 @@ PARAM_UPSAMP = 10
 os.makedirs(os.path.dirname(OUT_PATH), exist_ok=True)
 
 # %% generate data
-np.random.seed(42)
-ds = generate_data(
-    dpath=OUT_PATH,
-    ncell=100,
-    upsample=PARAM_UPSAMP,
-    dims={"height": 256, "width": 256, "frame": 2000},
-    sig_scale=1,
-    sz_mean=3,
-    sz_sigma=0.6,
-    sz_min=0.1,
-    tmp_P=np.array([[0.998, 0.002], [0.75, 0.25]]),
-    tmp_tau_d=PARAM_TAU_D,
-    tmp_tau_r=PARAM_TAU_R,
-    bg_nsrc=0,
-    bg_tmp_var=0,
-    bg_cons_fac=0,
-    bg_smth_var=0,
-    mo_stp_var=0,
-    mo_cons_fac=0,
-    post_offset=1,
-    post_gain=50,
-    save_Y=True,
-)
+for fname, upsamp in {"simulated.nc": 1, "simulated-upsamp.nc": PARAM_UPSAMP}.items():
+    np.random.seed(42)
+    ds = generate_data(
+        dpath=os.path.join(OUT_PATH, fname),
+        ncell=100,
+        upsample=upsamp,
+        dims={"height": 256, "width": 256, "frame": 2000},
+        sig_scale=1,
+        sz_mean=3,
+        sz_sigma=0.6,
+        sz_min=0.1,
+        tmp_P=np.array([[0.998, 0.002], [0.75, 0.25]]),
+        tmp_tau_d=PARAM_TAU_D,
+        tmp_tau_r=PARAM_TAU_R,
+        bg_nsrc=0,
+        bg_tmp_var=0,
+        bg_cons_fac=0,
+        bg_smth_var=0,
+        mo_stp_var=0,
+        mo_cons_fac=0,
+        post_offset=1,
+        post_gain=50,
+        save_Y=True,
+    )
