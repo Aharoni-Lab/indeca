dd1892089ab2dd3971e631b4f01b90e9d76e2526
phildong <35715936+phildong@users.noreply.github.com>
2024-10-25 10:55:48 -0400

    update 5best est results using direct l0

--- scripts/s01_benchmark_pipeline.py
+++ scripts/s01_benchmark_pipeline.py
@@ -28,8 +28,8 @@ IN_PATH = {
     "org": "./intermediate/simulated/simulated-exp-samp.nc",
     "upsamp": "./intermediate/simulated/simulated-exp-upsamp.nc",
 }
-INT_PATH = "./intermediate/benchmark_pipeline_100cell_est"
-FIG_PATH = "./figs/benchmark_pipeline_100cell_est"
+INT_PATH = "./intermediate/benchmark_pipeline_5best_est"
+FIG_PATH = "./figs/benchmark_pipeline_5best_est"
 PARAM_TAU_D = 6
 PARAM_TAU_R = 1
 PARAM_UPSAMP = 10
@@ -49,7 +49,7 @@ for up_type, up_factor in {"org": 1}.items():
     sim_ds = xr.open_dataset(IN_PATH[up_type])
     C_gt = sim_ds["C"].dropna("frame", how="all")
     # C_gt = norm_per_cell(C_gt)
-    subset = C_gt.coords["unit_id"]
+    subset = C_gt.coords["unit_id"][-5:]
     np.random.seed(42)
     sig_lev = xr.DataArray(
         np.sort(
