e0e93d75804cddbe0f18f424bfd40dc4619afe20
phildong <35715936+phildong@users.noreply.github.com>
2024-12-19 12:17:36 -0500

    add io routine for realds

---
+++ scripts/routine/io.py
@@ -0,0 +1,49 @@
+import os
+
+import fsspec
+import pandas as pd
+from scipy.io import loadmat
+
+
+def download_realds(target_path, remote_path="Ground_truth/"):
+    fs = fsspec.filesystem("github", org="HelmchenLabSoftware", repo="Cascade")
+    fs.get(remote_path, target_path, recursive=True)
+
+
+def load_gt_mat(matfile, varname="CAttached"):
+    dat = loadmat(matfile, simplify_cells=True)[varname]
+    fluo_df = (
+        pd.DataFrame({k: v for k, v in dat.items() if k in ["fluo_time", "fluo_mean"]})
+        .sort_values("fluo_time")
+        .reset_index()
+        .rename(columns={"index": "frame"})
+    )
+    ap_df = pd.DataFrame({"ap_time": dat["events_AP"] / 1e4}).sort_values("ap_time")
+    ap_df = pd.merge_asof(
+        ap_df,
+        fluo_df[["frame", "fluo_time"]],
+        left_on="ap_time",
+        right_on="fluo_time",
+        direction="nearest",
+    )
+    fluo_df = fluo_df.merge(
+        ap_df.groupby("frame")["ap_time"].count().rename("ap_count"),
+        on="frame",
+        how="left",
+    )
+    fluo_df["ap_count"] = fluo_df["ap_count"].fillna(0).astype(int)
+    return fluo_df, ap_df
+
+
+def load_gt_ds(ds_path):
+    fluo = []
+    for icell, matfile in enumerate(
+        filter(lambda fn: fn.endswith(".mat"), os.listdir(ds_path))
+    ):
+        fluo_df, _ = load_gt_mat(os.path.join(ds_path, matfile))
+        fluo_df["cell_id"] = icell
+        fluo.append(fluo_df)
+    fluo = pd.concat(fluo).set_index(["cell_id", "frame"])
+    C_true = fluo["fluo_mean"].rename("C_true").to_xarray()
+    S_true = fluo["ap_count"].rename("S_true").to_xarray()
+    return C_true, S_true
--- scripts/s00_benchmark_realds.py
+++ scripts/s00_benchmark_realds.py
@@ -1,6 +1,13 @@
 # %% import and definitions
-import fsspec
+import os
 
-# %% download
-fs = fsspec.filesystem("github", org="HelmchenLabSoftware", repo="Cascade")
-fs.get("Ground_truth/", "./data/realds/", recursive=True)
+from routine.io import download_realds, load_gt_ds
+
+LOCAL_DS_PATH = "./data/realds/"
+
+# %% download dataset
+if not os.path.exists(LOCAL_DS_PATH):
+    download_realds(LOCAL_DS_PATH)
+
+# %% load data
+C_true, S_true = load_gt_ds(os.path.join(LOCAL_DS_PATH, "DS01-OGB1-m-V1"))
