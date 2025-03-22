60ac4d7e71fe5f6ff712b7eb647dcd9027b31035
phildong <35715936+phildong@users.noreply.github.com>
2025-03-22 18:10:17 -0400

    add cnmf pipeline in demo

--- tests/test_pipeline.py
+++ tests/test_pipeline.py
@@ -7,6 +7,7 @@ import xarray as xr
 
 from minian_bin.deconv import construct_R
 from minian_bin.pipeline import pipeline_bin
+from minian_bin.simulation import find_dhm
 
 from .testing_utils.cnmf import pipeline_cnmf
 from .testing_utils.io import download_realds, load_gt_ds
@@ -96,6 +97,9 @@ class TestDemoPipeline:
     ):
         # act
         Y, ap_df, fluo_df = fixt_realds
+        C_cnmf, S_cnmf, tau_cnmf = pipeline_cnmf(
+            np.atleast_2d(Y), up_factor=1, est_noise_freq=0.06, sps_penal=0
+        )
         (
             C_bin,
             S_bin,
@@ -119,18 +123,31 @@ class TestDemoPipeline:
             spawn_dashboard=False,
         )
         # save results
+        iter_df = iter_df.set_index(["iter", "cell"])
         res_df = []
         for i_iter, sbin in enumerate(S_bin_iter):
             for iu, uid in enumerate(np.atleast_1d(Y.coords["unit_id"])):
                 sb = sbin[iu, :]
-                cur_ap = ap_df.loc[uid]
-                cur_fluo = fluo_df.loc[uid]
-                sb_idx = np.where(sb)[0] / param_upsamp
-                t_sb = np.interp(sb_idx, cur_fluo["frame"], cur_fluo["fluo_time"])
-                t_ap = cur_ap["ap_time"]
-                mdist, f1, prec, rec = assignment_distance(
-                    t_ref=np.array(t_ap), t_slv=np.array(t_sb), tdist_thres=1
-                )
+                tau_d, tau_r = iter_df.loc[(i_iter, iu), ["tau_d", "tau_r"]]
+                try:
+                    (dhm0, dhm1), _ = find_dhm(
+                        True, np.array([tau_d, tau_r]), np.array([1, -1])
+                    )
+                except AssertionError:
+                    dhm0, dhm1 = 0, 0
+                if len(ap_df) > 0:
+                    cur_ap = ap_df.loc[uid]
+                    cur_fluo = fluo_df.loc[uid]
+                    sb_idx = np.where(sb)[0] / param_upsamp
+                    t_sb = np.interp(sb_idx, cur_fluo["frame"], cur_fluo["fluo_time"])
+                    t_ap = cur_ap["ap_time"]
+                    mdist, f1, prec, rec = assignment_distance(
+                        t_ref=np.atleast_1d(t_ap),
+                        t_slv=np.atleast_1d(t_sb),
+                        tdist_thres=1,
+                    )
+                else:
+                    mdist, f1, prec, rec = np.nan, 0, 0, 0
                 res_df.append(
                     pd.DataFrame(
                         [
@@ -143,6 +160,49 @@ class TestDemoPipeline:
                                 "f1": f1,
                                 "prec": prec,
                                 "rec": rec,
+                                "dhm0": dhm0,
+                                "dhm1": dhm1,
+                            }
+                        ]
+                    )
+                )
+        for iu, uid in enumerate(np.atleast_1d(Y.coords["unit_id"])):
+            for qthres in [0.25, 0.5, 0.75]:
+                sb = S_cnmf[iu, :] > qthres
+                tau_d, tau_r = tau_cnmf[iu, :]
+                try:
+                    (dhm0, dhm1), _ = find_dhm(
+                        True, np.array([tau_d, tau_r]), np.array([1, -1])
+                    )
+                except AssertionError:
+                    dhm0, dhm1 = 0, 0
+                if len(ap_df) > 0:
+                    cur_ap = ap_df.loc[uid]
+                    cur_fluo = fluo_df.loc[uid]
+                    sb_idx = np.where(sb)[0] / param_upsamp
+                    t_sb = np.interp(sb_idx, cur_fluo["frame"], cur_fluo["fluo_time"])
+                    t_ap = cur_ap["ap_time"]
+                    mdist, f1, prec, rec = assignment_distance(
+                        t_ref=np.atleast_1d(t_ap),
+                        t_slv=np.atleast_1d(t_sb),
+                        tdist_thres=1,
+                    )
+                else:
+                    mdist, f1, prec, rec = np.nan, 0, 0, 0
+                res_df.append(
+                    pd.DataFrame(
+                        [
+                            {
+                                "method": "cnmf",
+                                "use_all": "unit_id" in Y.dims,
+                                "unit_id": uid,
+                                "qthres": qthres,
+                                "mdist": mdist,
+                                "f1": f1,
+                                "prec": prec,
+                                "rec": rec,
+                                "dhm0": dhm0,
+                                "dhm1": dhm1,
                             }
                         ]
                     )
