af06943922e9b12f0dd75f0c4cf5b89e7bf0f32e
phildong <35715936+phildong@users.noreply.github.com>
2025-04-22 12:12:35 -0400

    add `test_solve_scale` with error weighting

--- tests/test_deconv.py
+++ tests/test_deconv.py
@@ -6,7 +6,7 @@ import pytest
 from minian_bin.AR_kernel import AR2tau, tau2AR
 from minian_bin.deconv import DeconvBin
 
-from .conftest import fixt_deconv, fixt_realds
+from .conftest import fixt_deconv, fixt_realds, fixt_y
 from .testing_utils.metrics import (
     assignment_distance,
     compute_metrics,
@@ -199,6 +199,91 @@ class TestDeconvBin:
         fig.write_html(test_fig_path_html)
         # assert
         assert (s_bin == s).all()
+
+    @pytest.mark.parametrize("taus", [(6, 1), (10, 3)])
+    @pytest.mark.parametrize("upsamp", [1])
+    @pytest.mark.parametrize("ns_lev", [0, 0.2, 0.5])
+    @pytest.mark.parametrize("rand_seed", np.arange(3))
+    @pytest.mark.parametrize("penalty", [None])
+    @pytest.mark.parametrize("err_weighting", [None, "corr"])
+    def test_solve_scale(
+        self,
+        taus,
+        upsamp,
+        ns_lev,
+        rand_seed,
+        penalty,
+        err_weighting,
+        test_fig_path_svg,
+        test_fig_path_html,
+    ):
+        # act
+        y, c_true, c_org, s_true, s_org, scale = fixt_y(
+            taus=taus, upsamp=upsamp, rand_seed=rand_seed, ns_lev=ns_lev
+        )
+        taus_up = np.array(taus) * upsamp
+        _, _, p = AR2tau(*tau2AR(*taus_up), solve_amp=True)
+        deconv = DeconvBin(
+            y=y,
+            tau=taus,
+            ps=(p, -p),
+            penal=penalty,
+            err_weighting=err_weighting,
+        )
+        deconv_nowt = DeconvBin(
+            y=y,
+            tau=taus,
+            ps=(p, -p),
+            penal=penalty,
+            err_weighting=None,
+        )
+        err_wt = deconv.err_wt.squeeze()
+        s_free, b_free = deconv_nowt.solve(amp_constraint=False)
+        scl_ub = np.ptp(s_free)
+        res_df = []
+        for scl in np.linspace(0, scl_ub, 100)[1:]:
+            deconv.update(scale=scl)
+            deconv_nowt.update(scale=scl)
+            _, _, _, obj = deconv.solve_thres(scaling=False)
+            sbin, cbin, _, _ = deconv_nowt.solve_thres(scaling=False)
+            mdist, f1, prec, rec = assignment_distance(
+                s_ref=s_true, s_slv=sbin, tdist_thres=3
+            )
+            res_df.append(
+                pd.DataFrame(
+                    [
+                        {
+                            "scale": scl,
+                            "objs": obj,
+                            "mdist": mdist,
+                            "f1": f1,
+                            "prec": prec,
+                            "recall": rec,
+                        }
+                    ]
+                )
+            )
+        res_df = pd.concat(res_df, ignore_index=True)
+        opt_s, opt_c, cur_scl, cur_obj, cur_penal, iterdf = deconv.solve_scale(
+            return_met=True
+        )
+        # plotting
+        fig = plot_met_ROC_scale(res_df, iterdf, cur_scl)
+        fig.savefig(test_fig_path_svg)
+        fig = go.Figure()
+        fig.add_traces(
+            plot_traces(
+                {
+                    "y": y.squeeze(),
+                    "s_true": s_true.squeeze(),
+                    "c_true": c_true.squeeze(),
+                    "opt_s": opt_s.squeeze(),
+                    "opt_c": opt_c.squeeze(),
+                    "err_wt": err_wt,
+                }
+            )
+        )
+        fig.write_html(test_fig_path_html)
 
 
 @pytest.mark.slow
