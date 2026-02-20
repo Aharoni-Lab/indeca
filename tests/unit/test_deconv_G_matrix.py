06def209bc99545ab5c78fd540bfc93fb0c4f00d
kaghi <krishaaghi@gmail.com>
2026-02-20 15:16:06 -0800

    Fixing test_deconv_G_matrix unit test error.

--- tests/unit/test_deconv_G_matrix.py
+++ tests/unit/test_deconv_G_matrix.py
@@ -1,6 +1,7 @@
 import numpy as np
 
 from indeca.core.deconv import DeconvBin
+from indeca.core.deconv.deconv import InputParams
 from indeca.core.simulation import tau2AR
 
 
@@ -19,6 +20,7 @@ def test_G_matrix_matches_shifted_ar_difference_equation():
     theta = np.array(tau2AR(10.0, 3.0))
 
     deconv = DeconvBin(
+      InputParams(
         y_len=T,
         theta=theta,
         coef_len=3,
@@ -27,6 +29,7 @@ def test_G_matrix_matches_shifted_ar_difference_equation():
         use_base=False,
         norm="l2",
         penal=None,
+      )
     )
 
     # Use the full, unmasked operator for determinism.
